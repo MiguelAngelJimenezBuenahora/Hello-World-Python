e5d4e1c6229fc08c4add6f556f88918f2a13dc05
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-29 11:19:37 -0500

    Date Types on python

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-name = input('Inserte el nombre: ')
-message = f"hello world {name}"
-print(message)
+fullname = "Miguel Angel Castro Escamilla"     #Date type string
+gender = "M"    #Date type char(Character)
+age = 22    #Date type Int
+height = 1.687   #Date type Double(Doble)"
+trainer = True  #Date type Boolean
+
+#<Conjntos>
+hobbies = ["Programar",'Cocinar',"""WOW Horda"""]   #Date Type Array/List
+direction = (dict)["country ": "Colombia", "State": "Santander"]    #Date type object
+
+print(f"""Teacher's Name: {fullname}
+Teacher's Age: {age}
+Teacher's Gender: {gender}
+Teacher's Height: {height}
+Teacher's Trainer: {trainer}""")      #The correct form does
+
+#this is how do you can does the date types using number with letter
+#FirstName = "Miguel"
+
